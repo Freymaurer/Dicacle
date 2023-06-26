b25e2a422e8106ffaebe59c488f29c3e482b8b57
Kevin F <Freymaurer@gmx.de>
2023-06-26 07:58:29 +0200

    Update vite build index.js basepath

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  root: "./src",
-  build: {
+    plugins: [react()],
+    base: "./",
+    root: "./src",
+    build: {
     outDir: "../docs",
-  }
+    }
 })
